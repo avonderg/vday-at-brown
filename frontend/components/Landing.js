ecd4a5d0d77ffc73b64dc9c08a7cdc80e61010cd
Samantha Gundotra <40836349+samgundotra@users.noreply.github.com>
2022-02-07 12:38:22 -0500

    pink landing page

--- frontend/components/Landing.js
+++ frontend/components/Landing.js
@@ -9,7 +9,7 @@ export default function () {
         <Navbar isLanding/>
         <img src="/bears/smiling_red_bear.svg" className="rounded-lg mb-6 h-64 mx-auto"/>
         <div className="text-center">
-            <h1 className="text-3xl sm:text-5xl font-bold text-gray-900">Valentine's Day<span className="font-sans text-gray-500 font-light">@</span>Brown</h1>
+            <h1 className="text-3xl sm:text-5xl font-bold text-pink-400">Valentine's Day<span className="font-sans text-pink-200 font-light">@</span>Brown</h1>
             <p className="text-xl text-gray-500">Send someone you love a message this Valentine's Day! All messages are anonymous and will be sent out February 14th.</p>
             {/* <div className="mt-4"> */}
                 {/* <div className="contactme" id ="contact">
