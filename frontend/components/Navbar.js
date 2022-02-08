dabe333b75455a51e95821c6eacf60c6ab1cb441
Samantha Gundotra <40836349+samgundotra@users.noreply.github.com>
2022-02-07 23:38:29 -0500

    d '

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Navbar({isLanding, isMessage}) {
-    const linkStyling = `text-pink-600 hover:text-pink-400 focus:text-pink-400 ${!isLanding && "hidden sm:inline"}`
+    const linkStyling = `font-semibold text-pink-400 hover:text-pink-300 focus:text-pink-300 ${!isLanding && "hidden sm:inline"}`
 
     return <div className="absolute top-0 right-0 p-4 w-full flex justify-between items-center font-display bg-white z-50">
         <Link href="/">
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -3,16 +3,16 @@ import Navbar from "../components/Navbar";
 import Link from "next/link";
 
 const team = [{
-    name: "Sam G",
-    role: "Frontend/Backend Developer",
-    image: "/bears/nathan_bear.svg"
+    name: "anon (✿◠‿◠)",
+    role: "",
+    image: "/bears/sam_bear.svg"
     // linkedIn: "www.linkedin.com/in/nthnluu",
     // github: "https://github.com/nthnluu",
     // website: "https://nthnluu.com"
 }, {
-    name: "Alex V",
-    role: "Frontend Designer/Developer",
-    image: "https://ventureatbrown.com/bears/smiling_red_bear.svg"}]
+    name: "anon (✿ ♥‿♥)",
+    role: "",
+    image: "/bears/alex_bear.svg"}]
     // linkedIn: "https://www.linkedin.com/in/jasper-chen-1b8b98197",
     // github: "https://github.com/Jasch3n",
     // website: "https://jasch3n.github.io/mySite"
@@ -46,35 +46,21 @@ const About = () => {
                 <p className="text-xl text-gray-400 my-4">
                 Valentine’s Day@Brown was created by two hopeless romantics who decided it’s about time the Brown student body was shown a little love! After realizing there was no way to anonymously send valentines, we created this site as a fun way for students to show their appreciation for each other (or, ya know, confess their feelings to that special someone). Happy Valentine’s day everyone!
                 </p>
-                <a className="red-link text-xl" href="https://www.brownentrepreneurship.com/">Learn more about Brown EP &rarr;</a>
             </section>
             <section className="mt-16">
                 <h2 className="font-display text-2xl font-semibold mb-4 text-gray-800">Our Team</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                     {team.map((person, index) => <div className="font-medium leading-tight" key={index}>
-                        <img className="h-56 w-full object-cover rounded-lg shadow-sm"
+                        <embed className="h-56 w-full object-cover rounded-lg shadow-sm"
                                alt=""
                                src={person.image}/>
                         <h3 className="text-xl font-semibold text-gray-700 mt-4">{person.name}</h3>
                         <h4 className="text-lg text-gray-400">{person.role}</h4>
-                        {/* <div className="flex justify-start items-center space-x-4 mt-2 text-xl">
-                            <a className={buttonStyle} href={person.linkedIn} target="_blank">
-                                <i className="fab fa-linkedin"/>
-                            </a>
-
-                            <a className={buttonStyle} href={person.github} target="_blank">
-                                <i className="fab fa-github"/>
-                            </a>
-
-                            <a className={buttonStyle} href={person.website} target="_blank">
-                                <i className="fas fa-globe-americas"/>
-                            </a>
-                        </div> */}
+                        {}
                     </div>)}
 
                 </div>
             </section>
-            <div className="font-display text-gray-400 my-3">Graphics and illustrations by <a className="red-link" href="https://www.janicekhang.com/">Janice Khang</a></div>
         </div>
     </>
 }
